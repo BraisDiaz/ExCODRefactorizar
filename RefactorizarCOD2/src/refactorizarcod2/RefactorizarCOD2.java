8f6e55f5b1ac3f09fa8739610f4a559a03f31353
Brais Núñez <Brais Nez@DESKTOP-EI9I1J1>
2016-12-12 09:34:18 +0100

    -Copiado código ejercicio Refactorizar2 -Creada clase Factorial

---
+++ RefactorizarCOD2/src/refactorizarcod2/RefactorizarCOD2.java
@@ -0,0 +1,42 @@
+
+package refactorizarcod2;
+
+/**
+ *
+ * @author Brais Núñez
+ */
+public class RefactorizarCOD2 {
+
+    
+    public static void main(String[] args) {
+       int j;
+
+    int f;
+
+   j = 8;    
+
+   int i;
+
+   if (j == 0)
+
+f = 1;
+
+   else
+
+   {
+
+f = 1;
+
+for (i = j; i >= 1; i--)
+
+{
+    f = f * i;
+
+}
+   }    
+System.out.println(f); 
+}
+}
+    
+    
+
