dd268259dbc35e47ad2d7b80d3c2f8ff66a0aba3
Brais Núñez <Brais Nez@DESKTOP-EI9I1J1>
2016-12-12 09:18:10 +0100

    -Pasado el código a una clase creada al principio de nombre LoVivido. -Creados los métodos registrarPersona y calcularLoVivido. -Main: instanciado objeto de la clase LoVivido (persona1). -Main: realizadas llamadas a los métodos de la clase LoVivido.

--- RefactorizarCOD1/src/refactorizarcod1/LoVivido.java
+++ RefactorizarCOD1/src/refactorizarcod1/LoVivido.java
@@ -7,7 +7,25 @@ import javax.swing.JOptionPane;
  * @author Brais Núñez
  */
 public class LoVivido {
-   
+
+    int ano; String nombre;
+    
+    public void registrarPersona (){
+        
+        nombre = JOptionPane.showInputDialog ("Escriba su nombre: ");
+        ano = Integer.parseInt(JOptionPane.showInputDialog("Escriba su edad"));
+    }
+    public void calcularLoVivido (){
+        int mes = (ano*12); int dia = (ano*365); int hora = (dia*24);
+    
+    JOptionPane.showMessageDialog(null,  "Meses: "  +mes,"Numero de meses vividos de " +nombre,JOptionPane.INFORMATION_MESSAGE);
+
+    JOptionPane.showMessageDialog(null, "Días: " +dia,"Numero de días vividos de "+nombre,JOptionPane.INFORMATION_MESSAGE);
+
+    JOptionPane.showMessageDialog(null, "Horas: " +hora,"Numero de horas vividos de "+nombre,JOptionPane.INFORMATION_MESSAGE);
+ 
+    System.exit (0);
+}
 }
 
 
--- RefactorizarCOD1/src/refactorizarcod1/RefactorizarCOD1.java
+++ RefactorizarCOD1/src/refactorizarcod1/RefactorizarCOD1.java
@@ -11,19 +11,10 @@ public class RefactorizarCOD1 {
 
     public static void main(String[] args) {
         
-String nombre = JOptionPane.showInputDialog ("Escriba su nombre: ");
- 
-int ano = Integer.parseInt(JOptionPane.showInputDialog("Escriba su edad"));
-
-int mes = (ano*12); int dia = (ano*365); int hora = (dia*24);
-
- JOptionPane.showMessageDialog(null,  "Meses: "  +mes,"Numero de meses vividos de " +nombre,JOptionPane.INFORMATION_MESSAGE);
-
- JOptionPane.showMessageDialog(null, "Días: " +dia,"Numero de días vividos de "+nombre,JOptionPane.INFORMATION_MESSAGE);
-
- JOptionPane.showMessageDialog(null, "Horas: " +hora,"Numero de horas vividos de "+nombre,JOptionPane.INFORMATION_MESSAGE);
-
-     System.exit (0);
+        LoVivido persona1 = new LoVivido();
+        persona1.registrarPersona();
+        persona1.calcularLoVivido();
+       
 }
     }
     
