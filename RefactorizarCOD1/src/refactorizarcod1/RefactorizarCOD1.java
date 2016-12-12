19c4b07fae76f187d4b76df817f98fbd98620a7b
Brais Núñez <Brais Nez@DESKTOP-EI9I1J1>
2016-12-12 09:00:34 +0100

    -Declaradas todas las variables a la vez que se les da un valor y en menos lineas.

--- RefactorizarCOD1/src/refactorizarcod1/RefactorizarCOD1.java
+++ RefactorizarCOD1/src/refactorizarcod1/RefactorizarCOD1.java
@@ -10,21 +10,13 @@ import javax.swing.JOptionPane;
 public class RefactorizarCOD1 {
 
     public static void main(String[] args) {
-        String nombre;
-        int mes, dia, hora, ano;
-
-nombre = JOptionPane.showInputDialog ("Escriba su nombre: ");
+        
+String nombre = JOptionPane.showInputDialog ("Escriba su nombre: ");
  
-ano = Integer.parseInt(JOptionPane.showInputDialog("Escriba su edad"));
+int ano = Integer.parseInt(JOptionPane.showInputDialog("Escriba su edad"));
 
- 
-mes = (ano*12);
+int mes = (ano*12); int dia = (ano*365); int hora = (dia*24);
 
-dia = (ano*365);
-
-hora = (dia*24);
-
- 
  JOptionPane.showMessageDialog(null,  "Meses: "  +mes,"Numero de meses vividos de " +nombre,JOptionPane.INFORMATION_MESSAGE);
 
  JOptionPane.showMessageDialog(null, "Días: " +dia,"Numero de días vividos de "+nombre,JOptionPane.INFORMATION_MESSAGE);
